1f68f830a66704f0833af0e0a28b1b82c1d5c2a5
Ayush Jha <145919532+Sasquatch3663@users.noreply.github.com>
2026-05-16 11:29:39 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="software_defect_prediction",
+    version="0.1.1",
+    description="A Hybrid Machine Learning and Deep Learning Framework for Software Defect Prediction Using Feature Engineering and Imbalance Handling",
+    author="Ayush Jha",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.9",
+)
